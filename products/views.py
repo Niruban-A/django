1419fddc63136a895a82ebbc45b54d035c806212
Niruban-A <nirubanchipriyan14532gmail.com>
2025-02-25 18:59:20 +0530

    learning navigation and include

--- products/views.py
+++ products/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-    return render(request,"index.html",{})
+    return render(request,"index.html",{"name":"harini"})
 def  about(request):
     return render(request,"about.html",{})
