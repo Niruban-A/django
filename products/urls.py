c68ca9f7938c2228c8572fc6b7036c4e2067f827
Niruban-A <nirubanchipriyan14532gmail.com>
2025-02-26 11:51:02 +0530

    data rendering from the models

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.home_page,name="homepage"),
-    path("about/",views.about,name="aboutpage")
+    path("about/",views.about,name="aboutpage"),
+    path("model_data/",views.data_from_model,name="data rendering")
 ]
 
--- products/views.py
+++ products/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 
 # Create your views here.
 def home_page(request):
-    return render(request,"index.html",{"name":"harini"})
+    return render(request,"index.html",{"name":"niruban","my_list":[1,2,3,4,5,"niruban"]})
 def  about(request):
     return render(request,"about.html",{})
+def data_from_model(request):
+    obj=Product.objects.get(id=1)
+    context={"title":obj.title,
+             "description":obj.description}
+    return render(request,"about.html",context)
